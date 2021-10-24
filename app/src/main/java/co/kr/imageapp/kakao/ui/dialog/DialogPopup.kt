f1df8b59c0da081654bd64536830ac1c0efc14fd
parkjunho12 <ghdlwnsgh12@naver.com>
2021-10-25 00:53:17 +0900

    add filemanager

--- app/src/main/java/co/kr/imageapp/kakao/ui/dialog/DialogPopup.kt
+++ app/src/main/java/co/kr/imageapp/kakao/ui/dialog/DialogPopup.kt
@@ -1,20 +1,44 @@
 package co.kr.imageapp.kakao.ui.dialog
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.MediaScannerConnection
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.DialogFragment
 import co.kr.imageapp.kakao.const.KeyConst.VIDEO_TYPE
 import co.kr.imageapp.kakao.data.dto.mypage.ImageData
 import co.kr.imageapp.kakao.data.dto.search.SearchItem
 import co.kr.imageapp.kakao.databinding.DialogChoicePopupBinding
 import co.kr.imageapp.kakao.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,6 +52,19 @@ class DialogPopup(context: Context): DialogFragment() {
     val mContext = context
     private var isMypage = false
     private lateinit var choiceBinding: DialogChoicePopupBinding
+    val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // Do if the permission is granted
+            shareImages()
+        }
+        else {
+            // Do otherwise
+            Toast.makeText(requireContext(), "다운로드를 하려면 저장권한이 필요합니다.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     interface OnChoiceListener{
         fun clickCancel()
@@ -59,18 +96,65 @@ class DialogPopup(context: Context): DialogFragment() {
         return choiceBinding.root
     }
 
+    private fun shareImages() {
+        val sharingIntent = Intent(Intent.ACTION_SEND)
+        GlobalScope.launch(Dispatchers.IO) {
+            val imageUri = if (searchItem!!.searchType == VIDEO_TYPE) {
+                searchItem!!.thumbnail_url
+            } else {
+                searchItem!!.image_url
+            }
+            val bitmap = BitmapFactory.decodeStream(URL(imageUri).openStream())
+            sharingIntent.setType("image/jpg")
+            val fileManager = FileManager(requireContext())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                sharingIntent.putExtra(Intent.EXTRA_STREAM,  fileManager.saveImageInQ(bitmap))
+            } else {
+                sharingIntent.putExtra(Intent.EXTRA_STREAM,  fileManager.getImageUri(requireContext(),bitmap))
+            }
+            withContext(Dispatchers.Main) {
+                startActivity(Intent.createChooser(sharingIntent,"이미지 공유하기"))
+            }
+        }
+    }
+
+    private fun downLoadImages() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val imageUri = if (searchItem!!.searchType == VIDEO_TYPE) {
+                searchItem!!.thumbnail_url
+            } else {
+                searchItem!!.image_url
+            }
+            val bitmap = BitmapFactory.decodeStream(URL(imageUri).openStream())
+            val fileManager = FileManager(requireContext())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                fileManager.saveImageInQ(bitmap)
+            } else {
+                fileManager.getImageUri(requireContext(),bitmap)
+            }
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "${searchItem!!.title} 다운로드 되었습니다. 사진첩에서 확인해보세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
     private fun setClickEvent() = with(choiceBinding) {
         if (searchItem != null) {
-            sharePopupBtn.setOnClickListener {
-                val sharingIntent = Intent(Intent.ACTION_SEND)
-                val imageUri = if (searchItem!!.searchType == VIDEO_TYPE) {
-                    Uri.parse(searchItem!!.thumbnail_url)
+            downPopupBtn.setOnClickListener {
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 } else {
-                    Uri.parse(searchItem!!.image_url)
+                    downLoadImages()
                 }
-                sharingIntent.setType("image/png")
-                sharingIntent.putExtra(Intent.EXTRA_STREAM, imageUri)
-                startActivity(Intent.createChooser(sharingIntent,"이미지 공유하기"))
+            }
+            sharePopupBtn.setOnClickListener {
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                } else {
+                    shareImages()
+                }
             }
             openBrowserBtn.setOnClickListener {
                 if (searchItem != null) {
@@ -120,10 +204,10 @@ class DialogPopup(context: Context): DialogFragment() {
             }
         }
         if (isMypage) {
-            popupIconLin.toGone()
+            openBrowserBtn.toGone()
             choicePopupContent.text = "관련 링크로 이동하시겠습니까?"
         } else {
-            popupIconLin.toVisible()
+            openBrowserBtn.toVisible()
             choicePopupContent.text = "내 보관함에 추가 하시겠습니까?"
         }
     }
